f562f49f64ecf4adecd166cf7b10a51aba26e505
willyci <willyci@gmail.com>
2017-06-19 14:39:31 -0400

    first commit

---
+++ Hold2ChangeSAC.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hold2ChangeSAC : MonoBehaviour {
+
+    public SpriteLoader dicom_cube;//= SpriteLoader;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
